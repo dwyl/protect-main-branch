95c8c7af355b48c2d12c63aa5fcb837cb3589805
Danielwhyte <danwhy@gmail.com>
2017-11-10 16:44:31 +0000

    initial mix new setup

---
+++ lib/protect.ex
@@ -0,0 +1,18 @@
+defmodule Protect do
+  @moduledoc """
+  Documentation for Protect.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Protect.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/protect_test.exs
@@ -0,0 +1,8 @@
+defmodule ProtectTest do
+  use ExUnit.Case
+  doctest Protect
+
+  test "greets the world" do
+    assert Protect.hello() == :world
+  end
+end
